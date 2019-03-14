be5414897a9ff7f4b15e89dabdc640ae373899dd
Madeline Cameron <github@maddie.today>
2019-03-14 07:50:37 -0400

    fix(browser): Change url to https

--- browser/lib/findVideos.js
+++ browser/lib/findVideos.js
@@ -5,7 +5,7 @@ const request = require('superagent')
 module.exports = (self) => {
   self.addEventListener('message', (e) => {
     const data = JSON.parse(e.data)
-    request('http://tube.qnzl.co/api/search')
+    request('https://tube.qnzl.co/api/search')
       .query({ term: data.searchTerm, pageToken: data.token })
       .end((err, resp) => {
         if (err) {
